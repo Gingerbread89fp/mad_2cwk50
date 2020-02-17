b71300987b6db1e216f0d99dfcf7737c9b70bde9
Apple <apple@MacBook-Pro-di-Apple.local>
2020-02-17 15:28:37 +0000

    MAD-05: upload draft log-in page

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   ScrollView,
   View,
   Text,
+  Image,
   StatusBar,
   TextInput,
 } from 'react-native';
@@ -19,10 +20,21 @@ import {
 
 const styles = StyleSheet.create({
   input:{
-    flex:1,
-    flexDirection:'column',
-    alignItems:'center',
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    padding: 2,
     width: 300
+  },
+
+  image:{
+    width: 256, 
+    height: 256, 
+    borderRadius: 100, 
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginTop: 20,
+    marginBottom: 20
   }
 })
 
@@ -48,14 +60,19 @@ class Login extends Component{
   render(){
 
     return(
-      <View>
+      <View style={{flex:1, flexDirection:'column', alignItems:'center'}}>
+
+        <Image style={styles.image}></Image>
+
         <Text>User name:</Text>
-        <TextInput style={styles.input} />
+        <TextInput style={styles.input} 
+          value={this.state.username} 
+          onChangeText={(username)=>this.setState(username)}/>
 
         <Text>Password:</Text>
-        <TextInput style={styles.input} />
+        <TextInput style={styles.input} value={this.state.password}/>
 
-
+        <Text>Log-in</Text>
         <Text>Register now</Text>
 
       </View>
@@ -66,3 +83,5 @@ class Login extends Component{
 
 
 }
+
+export default Login;
