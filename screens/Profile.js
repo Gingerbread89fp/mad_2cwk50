524a167ca4ff9fa3b9e6675be6e4096ba1aa3879
Francesca <venom89.fp@gmail.com>
2020-02-23 17:14:19 +0000

    MAD-20: add logout button and function

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, Alert } from 'react-native';
 
-class Profile extends Component{
-    render(){
-        return(
+import styles from '../styles/app_style'
+
+class Profile extends Component {
+
+    logout(){
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/logout')
+        .then((response) => {
+          Alert.alert("Logged out successfully ")
+        })
+        .then((response)=> this.props.navigation.navigate('Login'))
+        .catch((error)=>{
+          console.log(error)
+        })
+      }
+
+
+
+    render() {
+        return (
             <View>
                 <Text>Profile page</Text>
+                <TouchableOpacity
+                    style={styles.button_style}
+                    onPress={() => this.logout()}>
+                    <Text>LOGOUT</Text>
+                </TouchableOpacity>
             </View>
         )
     }
