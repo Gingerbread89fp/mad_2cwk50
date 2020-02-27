ea0a86f9200df78e6d575573903a38cae04fdbb2
Francesca <venom89.fp@gmail.com>
2020-02-27 14:27:41 +0000

    MAD-12: add components skeleton into Profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Alert } from 'react-native';
+import { Text, View, Image, Alert } from 'react-native';
+
+import CustomIcon from '../app_components/customizedComponents';
 
 import styles from '../styles/app_style'
 
 class Profile extends Component {
 
-    logout(){
-        return fetch('http://10.0.2.2:3333/api/v0.0.5/logout')
-        .then((response) => {
-          Alert.alert("Logged out successfully ")
-        })
-        .then((response)=> this.props.navigation.navigate('Login'))
-        .catch((error)=>{
-          console.log(error)
-        })
-      }
+  logout() {
+    return fetch('http://10.0.2.2:3333/api/v0.0.5/logout')
+      .then((response) => {
+        Alert.alert("Logged out successfully ")
+      })
+      .then((response) => this.props.navigation.navigate('Login'))
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
 
 
-    render() {
-        return (
-            <View>
-                <Text>Profile page</Text>
-                <TouchableOpacity
-                    style={styles.button_style}
-                    onPress={() => this.logout()}>
-                    <Text>LOGOUT</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <View style={styles.page_container}>
+          <Text style={styles.page_title}>Profile</Text>
+
+          <CustomIcon
+            name={'md-log-out'}
+            size={40}
+            color={'green'}
+            onPress={() => this.logout()}
+          />
+        </View>
+
+        <View style={styles.page_content}>
+          <Image style={styles.image_profile} ></Image>
+          <View>
+            <Text style={styles.label}>First Name:</Text>
+            <Text style={styles.label}>Last Name:</Text>
+            <Text style={styles.label}>Email:</Text>
+          </View>
+        </View>
+
+        <View style={styles.page_container}>
+          <Text style={styles.page_title}>Followers</Text>
+          <Text style={styles.page_title}>Following</Text>
+        </View>
+
+      </View>
+    )
+  }
 }
 
 export default Profile;
