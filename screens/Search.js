cc15de8f4978523b91209ced7c8145b97b872911
Francesca <venom89.fp@gmail.com>
2020-02-29 21:39:16 +0000

    MAD-debug: fix the follow function passing user_id correctly and adding the token to auth action

--- screens/Search.js
+++ screens/Search.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, TouchableOpacity, Alert, FlatList } from 'react-native';
-import { Avatar } from "react-native-elements";
+import { Text, View, TextInput, Alert, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage'
 
+import styles from '../styles/app_style';
 import CustomIcon from '../app_components/customizedComponents';
 
 class Search extends Component{
@@ -9,15 +10,20 @@ class Search extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            given_name: '',
-            family_name: '',
-            user_id:'',
+            input_text: '',
+            toke:'',
             userList: []
         }
     }
 
+    static navigationOptions = ({
+        title: 'Search',
+        headerTitleStyle: styles.page_title,
+        headerStyle: {height: 64, marginBottom: 12}
+    });
+
     searchUsers() {
-        return fetch('http://10.0.2.2:3333/api/v0.0.5/search_user?q='+this.state.given_name)
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/search_user?q='+this.state.input_text)
         .then((response) => response.json())
         .then((responseJson) => {
             this.setState({
@@ -30,34 +36,46 @@ class Search extends Component{
         })
     }
 
-    followUser(){
-        return fetch('http://10.0.2.2:3333/api/v0.0.5/user/'+this.state.user_id+'/follow')
-        .then((response) => {
-            Alert.alert("user followed successfully ")
-        })
-        .catch((error)=>{
-        console.log(error)
-        })
+    componentDidMount() {
+        this.searchUsers();
     }
 
     displayData(item){
         return(
             <View style={styles.page_content}>
-                <Avatar
-                    size='medium'
-                    rounded
-                    icon={{name:'user', type: 'font-awesome'}}
-                    style={styles.avatar} 
-                />
-                <CustomIcon name={'md-person'} size={36} color={'green'} />
+                
                 <Text style={styles.label}>{item.given_name} {item.family_name}</Text>
-                <TouchableOpacity
-                    style={styles.button_style}
-                    onPress={() => this.followUser()}>
-                    <Text>+ FOLLOW</Text>
-                </TouchableOpacity>
+
+                <CustomIcon 
+                    name={'account-plus'} 
+                    size={36} 
+                    color={'green'} 
+                    onPress={() => AsyncStorage.getItem('token', (err, result) =>{
+                        if(result !=null){
+                            this.setState({ token: result });
+                            this.followUser(item.user_id)
+                        }
+                        else{Alert.alert("Please login to follow other chitters")}
+                    })}/>
             </View>
         )
+    }
+
+
+    followUser(user_id){
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/user/'+user_id+'/follow', {
+            method: 'POST',
+            headers: { 
+                "Content-Type": "application/json", 
+                "X-Authorization": JSON.parse(this.state.token)
+            }})
+        .then((response) => {
+            Alert.alert("user followed successfully ");
+           
+        })
+        .catch((error)=>{
+        console.log(error)
+        })
     }
 
     render(){
@@ -71,17 +89,22 @@ class Search extends Component{
                 <View style={styles.page_container}>
                     <TextInput
                         style={styles.input}
-                        value={this.state.given_name}
-                        onChangeText={(given_name) => this.setState({ given_name })} />
-                    <CustomIcon name={'md-search'} size={36} color={'green'} onPress={() => this.searchUsers()}/>
+                        placeholder='Search other chitters...'
+                        value={this.state.input_text}
+                        onChangeText={(input_text) => this.setState({ input_text })} />
+
+                    <CustomIcon 
+                        name={'magnify'} 
+                        size={36} color={'green'} 
+                        onPress={() => this.searchUsers()}/>
                 </View>
                 
 
                 <View>
                     <FlatList
-                    data = {this.state.userList}
-                    renderItem={({item, index}) => this.displayData(item, index)}
-                    keyExtractor={({given_name}, index) => given_name}
+                        data = {this.state.userList}
+                        renderItem={({item, index}) => this.displayData(item)}
+                        keyExtractor={({given_name}, index) => given_name}
                     />
                 </View>
 
