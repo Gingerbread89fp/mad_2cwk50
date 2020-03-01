31b7a93e9839500830a7e4824cb28f5df8000547
Francesca <venom89.fp@gmail.com>
2020-03-01 01:50:38 +0000

    MAD-refactor: correct error in token spelling

--- screens/Search.js
+++ screens/Search.js
@@ -11,7 +11,7 @@ class Search extends Component{
         super(props);
         this.state = {
             input_text: '',
-            toke:'',
+            token:'',
             userList: []
         }
     }
