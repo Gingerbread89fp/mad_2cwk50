63aa9ecd43d2c6f2b660656b8775137c96367dcd
Francesca <venom89.fp@gmail.com>
2020-03-05 21:48:15 +0000

    MAD-46: enable to post picture

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { RNCamera } from 'react-native-camera';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import CustomIcon from '../app_components/customizedIconButton';
 
 class Camera extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            chit_id: 0,
+        }
+    }
 
     static navigationOptions= {
         headerTitleStyle: styles.page_title,
@@ -14,28 +22,38 @@ class Camera extends Component {
 
     async takePicture() {
         if (this.camera) {
-            const options = { quality: 0.5, base64: true, pauseAfterCapture: false };
-            const data = await this.camera.takePictureAsync(options);
-            console.log(data.uri);
+            const settings = { quality: 0.5, base64: true, pauseAfterCapture: false };
+            const picture = await this.camera.takePictureAsync(settings);
+            console.log(picture.uri);
 
-            return fetch('http://10.0.2.2:3333/api/v0.0.5/chits', {
+            return fetch('http://10.0.2.2:3333/api/v0.0.5/chits/'+this.state.chit_id+'/photo', {
                 method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",
+                    "Content-Type": "image/jpeg",
                     "X-Authorization": JSON.parse(this.state.token)
                 },
-                body: JSON.stringify({
-                    timestamp: 0,
-                    chit_content: this.state.chit_content
-                })
-
+                body: picture
             })
-            .then((response) => navigation.navigate('Home'))
+            .then((reposnse) => Alert.alert('Image uploaded successfully'))
+            .then((response) => this.props.navigation.navigate('Home'))
             .catch((error) => {
                 console.log(error)
             })
         }
     };
+
+    componentDidMount(){
+        AsyncStorage.getItem('token', (err, result) =>{
+            this.setState({token: result})
+        })
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
+        .then((response) => response.json())
+        .then((responseJson) =>{
+            var lastChit = responseJson.length
+            this.setState({chit_id: lastChit})
+            console.log('**** id ', this.state.chit_id)
+        })
+    }
 
     render() {
         return (
@@ -49,7 +67,7 @@ class Camera extends Component {
                     <CustomIcon
                         name={'camera-outline'}
                         size={60}
-                        color={'black'}
+                        color={'white'}
                         onPress={this.takePicture.bind(this)}
                     />
                 </View>
